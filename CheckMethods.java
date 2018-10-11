6f24be2cb353be876359aafc9e333442923b90d6
Diotr <35659983+Diotr@users.noreply.github.com>
2018-10-11 21:31:32 +0100

    Add files via upload

--- CheckMethods.java
+++ CheckMethods.java
@@ -8,11 +8,27 @@ package cos;
 /**
  *
  * @author Diotr
- */
-public class CheckMethods {
-    public static void check(int godzinyalarm, int minutyalarm, int sekundyalarm ){
+ */public class CheckMethods {
+       int alhour=0;
+       int alminute=0;
+       int alsec=0;
        
-       if((godzinyalarm==21)&&(minutyalarm==13)&&(sekundyalarm==23)){
+       
+       
+     public static void setAlarm (){
+       
+    
+        System.out.println("Podaj godzinę alarmu: ");
+       
+    
+    }
+    
+     public static void check(int godzinyalarm, int minutyalarm, int sekundyalarm ){
+        
+   
+       
+
+       if((godzinyalarm==alhour)&&(minutyalarm==alminute)&&(sekundyalarm==alsec)){
            System.out.println("dzyn!");
            
        }
@@ -20,5 +36,5 @@ public class CheckMethods {
            System.out.println("czekaj");
        }
        }
-    
+   
 }
--- Cos.java
+++ Cos.java
@@ -3,25 +3,36 @@ package cos;
 import java.util.Date;
 import java.lang.Thread;
 import java.util.Calendar;
+import java.util.Scanner;
+
 
 public class Cos {
+
 
     
     public static void main(String[] args) throws InterruptedException {
         
-      CheckMethods test = new CheckMethods();
+       Scanner sc = new Scanner(System.in); 
+        
+       int godzina;
+       int minuty;
+       int sekundy;
        
+    
+       
+      //CheckMethods test = new CheckMethods();
+       CheckMethods.setAlarm();
        
       
         
         for(;;){
         Date data = new Date();
-        int godzina = data.getHours();
-        int minuty = data.getMinutes();
-        int sekundy = data.getSeconds();
+        godzina = data.getHours();
+        minuty = data.getMinutes();
+        sekundy = data.getSeconds();
         System.out.println("Godzina: "+godzina+"Minut: "+minuty+"Sekund: "+sekundy);
-        cos.CheckMethods.check(godzina,minuty,sekundy);
-         Thread.sleep(1000);}
+        CheckMethods.check(godzina,minuty,sekundy);
+        Thread.sleep(1000);}
        
         
         }
